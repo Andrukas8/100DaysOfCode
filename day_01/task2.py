8664d7ff2d26dda5e2bdf9d0b2736302fd90cc5e
T G <rukasav@gmail.com>
2025-06-29 21:46:39 +0300

    Add task2 file to the day_1 folder

---
+++ day_01/task2.py
@@ -0,0 +1,7 @@
+print("the length of your name is: " + str(len(input("Your name please: "))))
+
+print(len(input("Your name: ")))
+
+name = input("Your name again: ")
+length = len(name)
+print(length)
