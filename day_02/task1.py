ab14102cadc203144b2264b34b09ea8851eb466a
Andriy Rukas <rukasav@gmail.com>
2025-06-30 18:22:48 +0300

    Add day_2 tasks

---
+++ day_02/task1.py
@@ -0,0 +1,19 @@
+# subscripting
+print("Hello"[1])
+print("123" + "345")
+
+# integer = Whole number
+print(123 + 345)
+
+# Float
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
+len("Hello")
+print(type(123))
+print(type("abc"))
+print(type(123))
+print(type(3.14))
+print(type(True))
---
+++ day_02/task2.py
@@ -0,0 +1,3 @@
+# type casting
+print(int("123") + int("456"))
+print("Numberof letter in your name: " + str(len(input("Enter your name "))))
