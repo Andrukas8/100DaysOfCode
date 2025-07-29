1da8b91e2f7e520c1721b6210602260b27780ac5
Andriy Rukas <rukasav@gmail.com>
2025-07-29 18:03:08 +0300

    add day_10 code part 2

--- day_10/calculator.py
+++ day_10/calculator.py
@@ -1,3 +1,29 @@
+logo = """
+           _            _       _             
+          | |          | |     | |            
+  ___ __ _| | ___ _   _| | __ _| |_ ___  _ __ 
+ / __/ _` | |/ __| | | | |/ _` | __/ _ \| '__|
+| (_| (_| | | (__| |_| | | (_| | || (_) | |   
+ \___\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|   
+
+ _____________________
+|  _________________  |
+| |              0. | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|
+
+"""
+
+
 def add(n1, n2):
     return n1 + n2
 
@@ -11,6 +37,8 @@ def multiply(n1, n2):
 
 
 def divide(n1, n2):
+    if n2 == 0:
+        return "Division by zero"
     return n1 / n2
 
 
@@ -18,7 +46,37 @@ operations = {
     "+": add,
     "-": subtract,
     "*": multiply,
-    "/": divide
+    "/": divide,
 }
 
-print(operations["*"](5, 4))
+
+def calculator():
+    print(logo)
+    should_accumulate = True
+    num1 = float(input("What is the first number?: "))
+
+    while should_accumulate:
+
+        for symbol in operations:
+            print(symbol)
+
+        operation_symbol = input("Pick an operation: ")
+
+        num2 = float(input("What is the next number?: "))
+
+        answer = operations[operation_symbol](num1, num2)
+
+        print(f"\n{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(
+            f"\nType 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+
+
+calculator()
