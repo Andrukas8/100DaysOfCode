061e4f3980efa7c15165020f5c833fb6fcfa2cfe
Andriy Rukas <rukasav@gmail.com>
2025-09-26 13:00:19 +0300

    Add code day_19 snake starter

--- day_19/snake/main.py
+++ day_19/snake/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 
@@ -7,24 +8,20 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-segments = []
+snake = Snake()
 
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 
 game_is_on = True
-
 while game_is_on:
-    screen.update(0.1)
-    time.sleep(1)
-    for seg in segments:
-        seg.forward(20)
-
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
 screen.exitonclick()
