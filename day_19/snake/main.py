3393c99c339ec6d93b3300272f7b5596f69ef724
Andriy Rukas <rukasav@gmail.com>
2025-09-29 16:14:35 +0300

    Add day_19 snake continuation code

--- day_19/snake/main.py
+++ day_19/snake/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
 import time
 
 
@@ -10,6 +11,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,5 +25,11 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+# Detect collision with food
+    if snake.head.distance(food) < 15:
+        print("nom nom nom")
+        food.refresh()
+
 
 screen.exitonclick()
