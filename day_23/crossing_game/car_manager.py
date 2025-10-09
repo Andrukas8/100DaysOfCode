9e54762d29e3647f7dd553a58fe42258a7e84a34
Andriy Rukas <rukasav@gmail.com>
2025-10-09 14:46:34 +0300

    Add day_23 code crossing game finish

--- day_23/crossing_game/car_manager.py
+++ day_23/crossing_game/car_manager.py
@@ -3,12 +3,13 @@ import random
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
+MOVE_INCREMENT = 5
 
 
 class CarManager:
     def __init__(self):
         self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
 
     def create_car(self):
         random_chance = random.randint(1, 6)
@@ -23,7 +24,10 @@ class CarManager:
 
     def move_cars(self):
         for car in self.all_cars:
-            car.backward(STARTING_MOVE_DISTANCE)
+            car.backward(self.car_speed)
             if car.xcor() < -320:
                 self.all_cars.remove(car)
                 del car
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
--- day_23/crossing_game/scoreboard.py
+++ day_23/crossing_game/scoreboard.py
@@ -1,5 +1,25 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+FONT = ("Courier", 20, "normal")
+ALIGN = "left"
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 260)
+        self.update_level()
+
+    def increase_level(self):
+        self.level += 1
+        self.update_level()
+
+    def update_level(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align=ALIGN, font=FONT)
+
+    def game_over(self):
+        self.goto(-60, 0)
+        self.write("GAME OVER!", align=ALIGN, font=FONT)
