ef6e6d5517bd504b3c8e954887cb12930b371980
Andriy Rukas <rukasav@gmail.com>
2025-10-14 18:48:06 +0300

    Add snake game high score file

--- day_19/snake/scoreboard.py
+++ day_19/snake/scoreboard.py
@@ -8,17 +8,25 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.get_highscore()
         self.color("white")
         self.penup()
         self.goto(0, 275)
         self.hideturtle()
         self.update_score()
 
+    def get_highscore(self):
+        with open("data.txt", mode="r") as data:
+            return int(data.read())
+
+    def write_new_highscore(self, h_score):
+        with open("data.txt", mode="w") as data:
+            data.write(h_score)
+
     def update_score(self):
         self.clear()
         self.write(
-            f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+            f"Score: {self.score} High Score: {self.get_highscore()}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
@@ -28,4 +36,9 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.write_new_highscore(str(self.high_score))
         self.score = 0
+
+    def game_over(self):
+        self.update_score()
+        self.reset()
