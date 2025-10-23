6ab2066b6d3d23289b2ad85a7e109a107ec839d3
Andriy Rukas <rukasav@gmail.com>
2025-10-23 16:43:59 +0300

    Add day_25 code State Game

---
+++ day_25/50_States_Game/main.py
@@ -0,0 +1,32 @@
+import turtle
+import pandas as pd
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+image = "blank_states_img.gif"
+
+screen.addshape(image)
+
+turtle.shape(image)
+
+data = pd.read_csv("50_states.csv")
+
+all_states = data["state"].to_list()
+
+guessed_states = []
+
+while len(guessed_states) < 50:
+
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data["state"] == answer_state]
+        t.goto(state_data["x"].item(), state_data["y"].item())
+        t.write(state_data["state"].item())
+        guessed_states.append(state_data["state"].item())
+
+turtle.mainloop()
