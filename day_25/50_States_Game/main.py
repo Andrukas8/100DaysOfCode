9f2f90d65e474d0ddaa615a930e85ffeebed6512
Andriy Rukas <rukasav@gmail.com>
2025-10-24 12:04:27 +0300

    Add day_25 improvements to state game

--- day_25/50_States_Game/main.py
+++ day_25/50_States_Game/main.py
@@ -3,6 +3,12 @@ import pandas as pd
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 
+
+def get_user_answer():
+    return screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                            prompt="What's another state's name?").title()
+
+
 image = "blank_states_img.gif"
 
 screen.addshape(image)
@@ -17,10 +23,11 @@ guessed_states = []
 
 while len(guessed_states) < 50:
 
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
-                                    prompt="What's another state's name?").title()
+    answer_state = get_user_answer()
 
-    if answer_state in all_states:
+    if answer_state == "Exit":
+        break
+    elif answer_state in all_states:
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
@@ -28,5 +35,14 @@ while len(guessed_states) < 50:
         t.goto(state_data["x"].item(), state_data["y"].item())
         t.write(state_data["state"].item())
         guessed_states.append(state_data["state"].item())
+    else:
+        answer_state = get_user_answer()
 
-turtle.mainloop()
+# Generate states_to_learn.csv
+states_to_learn = []
+for state in all_states:
+    if state not in guessed_states:
+        states_to_learn.append(state)
+
+
+pd.Series(states_to_learn).to_csv("states_to_learn.csv")
