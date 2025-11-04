92ffe0a0c5adc2d22550f5aa049e6c3f2674ed5f
Andrukas8 <rukasav@gmail.com>
2025-11-04 15:09:40 +0200

    Add day_27 code tkinter practice

--- day_27/main.py
+++ day_27/main.py
@@ -9,8 +9,24 @@ window.minsize(width=500, height=300)
 # Label
 
 my_label = tk.Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label.pack()
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
 
-my_label.pack(side="left")
+# Button
 
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+button = tk.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+input = tk.Entry(width=10)
+input.pack()
 
 window.mainloop()
