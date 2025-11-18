fd15ec5db4daa21c48cdd0b5bae41f67a21c854b
Andrukas8 <rukasav@gmail.com>
2025-11-18 14:50:31 +0200

    Add day_28 pomodoro project continuation

--- day_28/main.py
+++ day_28/main.py
@@ -10,29 +10,75 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
 FONT_BUTTON = (FONT_NAME, 12, "normal")
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- #
+
+
+def reset_timer():
+    global reps
+    reps = 0
+    window.after_cancel(timer)
+    title_label.configure(text="Timer", fg=GREEN)
+    check_marks.config(text="")
+    canvas.itemconfig(timer_text, text="00:00")
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    global checks
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.configure(text="Break", fg=RED)
+
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.configure(text="Break", fg=PINK)
+
+    else:
+        count_down(work_sec)
+        title_label.configure(text="Work", fg=GREEN)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
 def count_down(count):
-
+    global timer
     mins = count // 60
     secs = count % 60
 
+    if secs == 0:
+        secs = "00"
+
+    elif secs < 10:
+        secs = f"0{secs}"
+
+    if mins < 10:
+        mins = f"0{mins}"
+
     timer = f"{mins}:{secs}"
     canvas.itemconfig(timer_text, text=timer)
+
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = reps // 2
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -59,10 +105,10 @@ start_button = tk.Button(text="Start", highlightthickness=0,
 start_button.grid(column=0, row=2)
 
 reset_button = tk.Button(text="Reset", highlightthickness=0,
-                         bg=PINK, fg="white", font=FONT_BUTTON)
+                         bg=PINK, fg="white", font=FONT_BUTTON, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_marks = tk.Label(text="✔", fg=GREEN, bg=YELLOW,
+check_marks = tk.Label(fg=GREEN, bg=YELLOW,
                        font=(FONT_NAME, 15, "normal"))
 check_marks.grid(column=1, row=3)
 
