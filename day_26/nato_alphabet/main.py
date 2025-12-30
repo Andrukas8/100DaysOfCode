d69b72efeac0b89de4fd2cbba65d208f922219a3
Andrukas8 <rukasav@gmail.com>
2025-12-30 16:48:47 +0200

    Add exception handling to phonetic app

--- day_26/nato_alphabet/main.py
+++ day_26/nato_alphabet/main.py
@@ -5,14 +5,19 @@ data = pd.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row["letter"]: row["code"]
                  for (index, row) in data.iterrows()}
 
-user_word = input("Enter a word: ").upper()
 
-print(user_word)
-print("-----------")
-answer = [phonetic_dict[letter] for letter in user_word]
+def generate_phonetics():
+    user_word = input("Enter a word: ").upper()
 
-for letter in answer:
-    print(letter)
+    try:
+        answer = [phonetic_dict[letter] for letter in user_word]
 
-print("-----------")
-print(user_word)
+    except KeyError as error_message:
+        print(f"Only letters please. {error_message}")
+        generate_phonetics()
+
+    else:
+        print(answer)
+
+
+generate_phonetics()
--- day_30/main.py
+++ day_30/main.py
@@ -22,11 +22,32 @@
 
 try:
     file = open("a_file.txt")
-    a_dictionary = {"key", "value"}
-    print(a_dictionary["sdsfefw"])
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
 except FileNotFoundError:
     print("There was an error")
     file = open("a_file.txt", "w")
     file.write("Something")
-except KeyError:
-    print("Key does not exist")
+except KeyError as error_message:
+    print(f"That key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed.")
+# Raising own exceptions
+    # raise TypeError("The message was an error I made up.")
+
+# Example
+height = float(input("Height: "))
+weight = float(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human height should not me over 3 m.")
+
+if weight > 500:
+    raise ValueError("Human weight should not be more than 500 kg.")
+
+bmi = round(weight / height ** 2, 2)
+print(f"BMI = {bmi}")
