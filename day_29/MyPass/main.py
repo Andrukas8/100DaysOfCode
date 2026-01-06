50a4e94d0a459e0cb94fec7304a4fa918fb51b60
Andrukas8 <rukasav@gmail.com>
2026-01-06 19:22:40 +0200

    Add find password ability and button

--- day_29/MyPass/main.py
+++ day_29/MyPass/main.py
@@ -48,18 +48,49 @@ def save():
         messagebox.showinfo(
             title="Oops", message="Please make sure you didn't leave any fields empty")
     else:
-        with open("data.json", "r") as data_file:
+        try:
             # Reading old data
-            data = json.load(data_file)
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+
+        except FileNotFoundError:
+            # Creating data file if it does not exist
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+
+        else:
             # Updating old data with new data
             data.update(new_data)
 
-        with open("data.json", "w") as data_file:
             # Saving updated data
-            json.dump(data, data_file, indent=4)
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
 
+        finally:
+            # Clearing the entry fields
             website_entry.delete(0, END)
             password_entry.delete(0, END)
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(
+                title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(
+                title="Error", message=f"No details for {website} found.")
 
     # ---------------------------- UI SETUP ------------------------------- #
 
@@ -72,36 +103,42 @@ window.config(padx=50, pady=50)
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0, column=1)
+canvas.grid(row=0, column=0, columnspan=3)
 
 # Labels
 website_label = Label(text="Website:")
-website_label.grid(row=1, column=0)
+website_label.grid(row=1, column=0, sticky='w')
 
 email_label = Label(text="Email/Username:")
-email_label.grid(row=2, column=0)
+email_label.grid(row=2, column=0, sticky='w')
 
 password_label = Label(text="Password:")
-password_label.grid(row=3, column=0)
+password_label.grid(row=3, column=0, sticky='w')
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1, sticky='w')
 website_entry.focus()
 
+
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.grid(row=2, column=1, columnspan=2, sticky='w')
 email_entry.insert(0, "email@email.com")
 
-password_entry = Entry()
-password_entry.grid(row=3, column=1)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1, sticky='w')
 
 # Buttons
-generate_password_button = Button(
-    text="Generate", command=generate_password)
-generate_password_button.grid(row=3, column=2)
 
-add_button = Button(text="Add", width=36, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", width=10, command=find_password)
+search_button.grid(row=1, column=1, columnspan=2, sticky='e')
+
+generate_password_button = Button(
+    text="Generate", width=10, command=generate_password)
+generate_password_button.grid(row=3, column=1, columnspan=2, sticky='e')
+
+
+add_button = Button(text="Add", width=28, command=save)
+add_button.grid(row=4, column=1, columnspan=2, sticky='w')
 
 window.mainloop()
